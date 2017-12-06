d16e4974c10721f295ae7372cd235bf486cb0c21
bobzz-zone <bobzz.zone@gmail.com>
2017-12-06 09:56:31 +0700

    aa

--- home_developer/home_developer/doctype/tanda_terima/tanda_terima.py
+++ home_developer/home_developer/doctype/tanda_terima/tanda_terima.py
@@ -7,4 +7,27 @@ import frappe
 from frappe.model.document import Document
 
 class TandaTerima(Document):
-	pass
+	def validate(self):
+		self.generate_in_words()			
+		if self.workflow_state != self.old_workflow :
+			self.generate_document_ledger()
+	def generate_in_words(self):
+		if self.utj :
+			self.terbilang_utj = frappe.utils.data.money_in_words(self.utj,"Rupiah","Sen")
+		else :
+			self.terbilang_utj = "-"
+	
+	def on_update_after_submit(self):
+		if self.workflow_state != self.old_workflow :
+			self.generate_document_ledger()
+	def generate_document_ledger(self):
+		dt = frappe.new_doc("Document Ledger")
+		dt.document_type = "Tanda Terima"
+		dt.document_name = self.name
+		dt.sales_person = self.sales_person
+		dt.document_status = self.workflow_state
+		dt.posting_date = self.posting_date
+		dt.value = self.utj
+		dt.transition_date = frappe.utils.today()
+		dt.save()
+		self.old_workflow = self.workflow_state
